edf031755fa36a91010656dc94225664d96fd70d
Mathys Clerget <mathysclerget+github@gmail.com>
2022-12-29 18:25:27 +0100

    feat: Square implementation

--- BattleShip/Square.cpp
+++ BattleShip/Square.cpp
@@ -1,1 +1,23 @@
 #include "Square.h"
+
+Square::Square(short const _x, short const _y) :x(_x), y(_y){
+	this->hit = false;
+}
+
+Square::~Square() {}
+
+short Square::getX() const {
+	return this->x; 
+}
+
+short Square::getY() const { 
+	return this->y; 
+}
+
+bool Square::isHit() const { 
+	return this->hit; 
+}
+
+void Square::setHit(bool const _h) { 
+	this->hit = _h; 
+}
--- BattleShip/Square.h
+++ BattleShip/Square.h
@@ -1,5 +1,47 @@
 #pragma once
+/// <summary>
+/// The square class represents a tuple of coordinates on the board.
+/// The square contains the `hit` information. Whether this square was drawn by a player or not.
+/// </summary>
 class Square
 {
+private:
+	short x;
+	short y;
+	bool hit;
+public:
+
+	/// <summary>
+	/// Default constructor of Square.
+	/// </summary>
+	/// <param name="_x">X coordinate.</param>
+	/// <param name="_y">Y coordinate.</param>
+	/// <author>MathysC</author>
+	Square(short const _x, short const _y);
+
+	/// <summary>
+	/// Destructor of Square.
+	/// </summary>
+	/// <author>MathysC</author>
+	~Square();
+
+	/// <returns>The x coordinate to get.</returns>
+	/// <author>MathysC</author>
+	short getX() const;
+	
+	/// <returns>The y coordinate to get.</returns>
+	/// <author>MathysC</author>
+	short getY() const;
+	
+	/// <returns> the hit value of the square to get.</returns>
+	/// <author>MathysC</author>
+	bool isHit() const;
+
+	/// <summary>
+	/// Set the hit value of the Square.
+	/// </summary>
+	/// <param name="_h">The new hit value.</param>
+	/// <author>MathysC</author>
+	void setHit(bool const _h);
 };
 
