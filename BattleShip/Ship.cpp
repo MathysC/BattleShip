3a8dcf85112470d27e17c9933e60da19e168eff5
Mathys Clerget <mathysclerget+github@gmail.com>
2022-12-30 01:05:15 +0100

    feat: Ship implementation

--- BattleShip/Ship.cpp
+++ BattleShip/Ship.cpp
@@ -1,1 +1,103 @@
+#include <assert.h>
 #include "Ship.h"
+
+Ship::Ship(Way const _way, size_t const _l, short const _x, short const _y):
+	way(_way), length(_l)
+{
+	assert(_l > 0);
+	switch (_way)
+	{
+	case Way::EAST: {
+		for (short current_x = _x; current_x < _x + _l; current_x++) {
+			this->squares.push_back(Square(current_x, _y));
+		}
+		break;
+	}
+	case Way::NORTH: {
+		for (short current_y = _y; current_y > _y - _l; current_y--) {
+			this->squares.push_back(Square(_x, current_y));
+		}
+		break;
+	}
+	case Way::SOUTH: {
+		for (short current_y = _y; current_y < _y + _l; current_y++) {
+			this->squares.push_back(Square(_x, current_y));
+		}
+		break;
+	}
+	case Way::WEST: {
+		for (short current_x = _x; current_x > _x - _l; current_x--) {
+			this->squares.push_back(Square(current_x, _y));
+		}
+		break;
+	}
+	default:
+		throw std::invalid_argument("'_way' argument isn’t supported.");
+		break;
+	}
+}
+
+Ship::~Ship() {}
+
+bool Ship::hit(short const _x, short const _y)
+{
+	// If the ship has already sunk, it cannot be hit anymore.
+	if (this->hasSunk()) { 
+		return false; 
+	}
+
+	// Get the starting point of the ship.
+	Square start = this->squares.at(0);
+	bool hit = false;
+	switch (this->way)
+	{
+	case Way::EAST: {
+		if ((_y == start.getY()) &&
+			(_x >= start.getX() && _x <= start.getX() + this->length)) {
+			this->squares.at(_x).setHit(true);
+			hit = true;
+		}
+		break;
+	}
+	case Way::NORTH: {
+		if ((_x == start.getX()) &&
+			(_y >= start.getY() - this->length && _y <= start.getY())) {
+			this->squares.at(_y).setHit(true);
+			hit = true;
+		}
+		break;
+	}
+	case Way::SOUTH: {
+		if ((_x == start.getX()) &&
+			(_y >= start.getY() && _y <= start.getY() + this->length)) {
+			this->squares.at(_y).setHit(true);
+			hit = true;
+		}
+		break;
+	}
+	case Way::WEST: {
+		if ((_y == start.getY()) &&
+			(_x >= start.getX() - this->length && _x <= start.getX())) {
+			this->squares.at(_x).setHit(true);
+			hit = true;
+		}
+		break;
+	}
+	default:
+		throw std::invalid_argument("'way' argument isn’t supported.");
+		break;
+	}
+
+	return hit;
+}
+
+bool Ship::hasSunk() const
+{
+	for (Square square : this->squares) {
+		if (!square.isHit()) {
+			return false;
+		}
+	}
+
+	return true;
+}
--- BattleShip/Ship.h
+++ BattleShip/Ship.h
@@ -1,4 +1,53 @@
 #pragma once
+#include <stdexcept>
+#include <vector>
+#include "Square.h"
+enum class Way {
+	EAST, 
+	NORTH,
+	SOUTH,
+	WEST,
+};
+
 class Ship
 {
+private:
+	Way way;
+	size_t length;
+	std::vector<Square> squares;
+
+public:
+	/// <summary>
+	/// Constructor of Ship.
+	/// Set the way the ship is facing, in order to create the squares needed with correct coordinates.
+	/// </summary>
+	/// <param name="_way">The way the ship is facing.</param>
+	/// <param name="_l">The length of the ship.</param>
+	/// <param name="_x">The x coordinate of the start of the ship.</param>
+	/// <param name="_y">The y coordinate of the start of the ship.</param>
+	/// <author>MathysC</author>
+	Ship(Way const _way, size_t const _l, short const _x, short const _y);
+
+	/// <summary>
+	/// Destructor of Ship.
+	/// </summary>
+	/// <author>MathysC</author>
+	~Ship();
+
+	/// <summary>
+	/// Hit the ship at the given coordinates.
+	/// </summary>
+	/// <param name="x">The x coordinate of the hit.</param>
+	/// <param name="y">The y coordinate of the hit.</param>
+	/// <returns>True if the ship is hit, else false.</returns>
+	/// <author>MathysC</author>
+	bool hit(short const _x, short const _y);
+
+	/// <summary>
+	/// Check if the ship is completely sunk or not.
+	/// </summary>
+	/// <returns>True if each squares of the Ship are hit, else false if at least one square is not hit yet.</returns>
+	/// <author>MathysC</author>
+	bool hasSunk() const;
 };
+
