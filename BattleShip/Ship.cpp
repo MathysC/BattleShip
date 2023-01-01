2fa5b7d9b3ddd77570aa9f6b403046e07453f278
Mathys Clerget <mathysclerget+github@gmail.com>
2023-01-02 00:38:18 +0100

    fix: hit method

--- BattleShip/Ship.cpp
+++ BattleShip/Ship.cpp
@@ -7,26 +7,26 @@ Ship::Ship(Way const _way, size_t const _l, short const _x, short const _y):
 	assert(_l > 0);
 	switch (_way)
 	{
-	case Way::EAST: {
+	case Way::EAST: { // x -> x + length && same y
 		for (short current_x = _x; current_x < _x + _l; current_x++) {
 			this->squares.push_back(Square(current_x, _y));
 		}
 		break;
 	}
-	case Way::NORTH: {
-		for (short current_y = _y; current_y > _y - _l; current_y--) {
+	case Way::NORTH: { // same x && y -> y - length
+		for (short current_y = _y; current_y < _y - _l; current_y--) {
 			this->squares.push_back(Square(_x, current_y));
 		}
 		break;
 	}
-	case Way::SOUTH: {
+	case Way::SOUTH: { // same x && y -> y + length
 		for (short current_y = _y; current_y < _y + _l; current_y++) {
 			this->squares.push_back(Square(_x, current_y));
 		}
 		break;
 	}
-	case Way::WEST: {
-		for (short current_x = _x; current_x > _x - _l; current_x--) {
+	case Way::WEST: { // x -> x - l -> same y
+		for (short current_x = _x; current_x < _x - _l; current_x--) {
 			this->squares.push_back(Square(current_x, _y));
 		}
 		break;
@@ -52,34 +52,48 @@ bool Ship::hit(short const _x, short const _y)
 	switch (this->way)
 	{
 	case Way::EAST: {
-		if ((_y == start.getY()) &&
-			(_x >= start.getX() && _x <= start.getX() + this->length)) {
+		short _end_x = start.getX() + this->length;
+		if (_y == start.getY() &&
+			(_x >= start.getX() && 
+				_x < _end_x)) {
+			hit = !this->squares.at(_x).isHit(); // If already hit then return false.
 			this->squares.at(_x).setHit(true);
-			hit = true;
 		}
 		break;
 	}
 	case Way::NORTH: {
-		if ((_x == start.getX()) &&
-			(_y >= start.getY() - this->length && _y <= start.getY())) {
-			this->squares.at(_y).setHit(true);
-			hit = true;
+		short _end_y = start.getY() - this->length;
+		if (_x == start.getX() && 
+			(_y > _end_y && 
+				_y <= start.getY())) {
+
+			// at = opposite value because the ship is facing the y-axis in the opposite direction
+			short _at = this->length - 1 - _y;
+			hit = !this->squares.at(_at).isHit(); // If already hit then return false.
+			this->squares.at(_at).setHit(true);
 		}
 		break;
 	}
 	case Way::SOUTH: {
-		if ((_x == start.getX()) &&
-			(_y >= start.getY() && _y <= start.getY() + this->length)) {
+		short _end_y = start.getY() + this->length;
+		if (_x == start.getX() &&
+			(_y >= start.getY() && 
+				_y < _end_y)) {
+			hit = !this->squares.at(_y).isHit(); // If already hit then return false.
 			this->squares.at(_y).setHit(true);
-			hit = true;
 		}
 		break;
 	}
 	case Way::WEST: {
-		if ((_y == start.getY()) &&
-			(_x >= start.getX() - this->length && _x <= start.getX())) {
-			this->squares.at(_x).setHit(true);
-			hit = true;
+		short _end_x = start.getX() - this->length;
+		if (_y == start.getY() &&
+			(_x > _end_x && 
+				_x <= start.getX())) {
+			
+			// at = opposite value because the ship is facing the x-axis in the opposite direction
+			short _at = this->length - 1 - _x;
+			hit = !this->squares.at(_at).isHit(); // If already hit then return false.
+			this->squares.at(_at).setHit(true);
 		}
 		break;
 	}
