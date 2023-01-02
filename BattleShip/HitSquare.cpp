9c14a4c97ea9e5d20ff7547ee26d7ac730390287
Mathys Clerget <mathysclerget+github@gmail.com>
2023-01-02 01:09:25 +0100

    feat(WIP): HitSquare state method

--- BattleShip/HitSquare.cpp
+++ BattleShip/HitSquare.cpp
@@ -1,1 +1,9 @@
 #include "HitSquare.h"
+
+HitSquare::HitSquare() {}
+
+HitSquare::~HitSquare() {}
+
+void HitSquare::treat(Board _b, short const _x, short const _y)
+{
+}
--- BattleShip/HitSquare.h
+++ BattleShip/HitSquare.h
@@ -1,7 +1,34 @@
 #pragma once
 #include "BoardState.h"
+
+/// <summary>
+/// The HitSquare state represent the main action on a BattleShip Board: Hit a square.
+/// </summary>
+/// <author>MathysC</author>
 class HitSquare :
     public BoardState
 {
+public:
+	/// <summary>
+	/// Default constructor of HitSquare State.
+	/// </summary>
+	/// <author>MathysC</author>
+	HitSquare();
+
+	/// <summary>
+	/// Default destructor of HitSquare State.
+	/// </summary>
+	/// <author>MathysC</author>
+	~HitSquare();
+
+
+	/// <summary>
+	/// Hit a square on the Board at the given coordinates.
+	/// </summary>
+	/// <param name="_b">The BattleShip Board.</param>
+	/// <param name="_x">X coordinate.</param>
+	/// <param name="_y">Y coordinate.</param>
+	/// <author>MathysC</author>
+	void treat(Board _b, short const _x, short const _y);
 };
 
