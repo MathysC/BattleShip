afd2e5ac6bc1b4877dba31cefaf5c0ddf50bc9bf
Mathys Clerget <mathysclerget+github@gmail.com>
2023-01-02 16:04:18 +0100

    feat: BoardTest Base

---
+++ BattleShipTest1/BoardTest.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BattleShipTest1
+{
+	TEST_CLASS(BoardTest)
+	{
+	public:
+		TEST_METHOD(TestGetShips) {}
+
+		TEST_METHOD(TestAddShip) {}
+
+		TEST_METHOD(TestHitSquare) {}
+
+		TEST_METHOD(TestIsWarOver) {}
+
+	};
+}
